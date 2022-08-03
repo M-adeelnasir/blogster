229d349169cc2fa3596f8805f174de2b8a632308
adeelkbw <adeelnasirkbw@gmail.com>
2022-08-03 16:17:54 +0500

    redis setup and get and set blogs in redis server

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 
+
 const Blog = mongoose.model('Blog');
 
 module.exports = app => {
@@ -14,9 +15,29 @@ module.exports = app => {
   });
 
   app.get('/api/blogs', requireLogin, async (req, res) => {
-    const blogs = await Blog.find({ _user: req.user.id });
 
+    const redis = require('redis');
+    const client = redis.createClient()
+    // client.on('error', (err) => console.log("Redis client error ", err))
+    // client.connect().then(() => console.log("Redis connected..."))
+    const util = require('util');
+
+    client.get = util.promisify(client.get)
+
+    //have already data in cache then serve from cache
+    const blogCache = await client.get(req.user.id)
+    if (blogCache) {
+      console.log("SERVERVING FROM CACHE");
+      return res.send(JSON.parse(blogCache))
+    }
+
+    //if no then get data from db and responed to client and then save it in cache
+    const blogs = await Blog.find({ _user: req.user.id });
     res.send(blogs);
+
+    console.log("SERVERVING FROM DB");
+    client.set(req.user.id, JSON.stringify(blogs))
+
   });
 
   app.post('/api/blogs', requireLogin, async (req, res) => {
