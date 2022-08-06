069c2810ff98f5b10b9a80f2de223952c8a60eff
adeelkbw <adeelnasirkbw@gmail.com>
2022-08-07 02:16:31 +0500

    checked the data in redis otherwise run query

--- services/cache.js
+++ services/cache.js
@@ -1,11 +1,31 @@
 const mongoose = require('mongoose');
+const redis = require('redis');
+const util = require('util');
 
 const exec = mongoose.Query.prototype.exec;
 
+const client = redis.createClient()
+client.get = util.promisify(client.get)
 
-mongoose.Query.prototype.exec = function () {
-    console.log("I am ran before the actual query executed")
+mongoose.Query.prototype.exec = async function () {
 
-    return exec.apply(this, arguments)
+    // console.log("I am ran before the actual query executed")
+    // console.log(this.getQuery());
+    // console.log(this.mongooseCollection.name);
+
+    const key = JSON.stringify(Object.assign({}, this.getQuery(), { collection: this.mongooseCollection.name }))
+
+    //if we have value against the 'key' in redis
+    const cachedValue = await client.get(key)
+    //if we have then return it
+    if (cachedValue) {
+        console.log(cachedValue);
+    }
+    // if not then issue the mongo query and store the data in redis
+
+
+
+    const result = await exec.apply(this, arguments)
+    console.log("Result ===>", result);
 }
 
