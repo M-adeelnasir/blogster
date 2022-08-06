57529b6e0fa918a700cd413e4a88b0a6f85502ac
adeelkbw <adeelnasirkbw@gmail.com>
2022-08-07 02:50:39 +0500

    store and retive the document from redis --redis

--- services/cache.js
+++ services/cache.js
@@ -19,13 +19,29 @@ mongoose.Query.prototype.exec = async function () {
     const cachedValue = await client.get(key)
     //if we have then return it
     if (cachedValue) {
-        console.log(cachedValue);
+        // console.log(cachedValue);
+        // return JSON.parse(cachedValue)  //this is not going to work bcz it not a mongoose object first we have to convert it into a mongo object
+        const doc = JSON.parse(cachedValue)
+        // this also not going to work bcz model can only work with signle document bul redis rreturn an 'Array'
+        // return new this.model(docs)
+
+        console.log("Value From Cache");
+
+        //solution
+        Array.isArray(doc)
+            ?
+            doc.map(d => new this.model(d))
+            :
+            new this.model(doc)
+
+
     }
     // if not then issue the mongo query and store the data in redis
-
-
-
     const result = await exec.apply(this, arguments)
-    console.log("Result ===>", result);
+    // console.log("Result ===>", result);
+
+    client.set(key, JSON.stringify(result))
+
+    return result
 }
 
